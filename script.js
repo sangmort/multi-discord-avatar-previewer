1e8f44b1ea3283a78dbbd4568bb977e3f164b3a3
sangmort <22422853+sangmort@users.noreply.github.com>
2023-05-30 16:05:36 -0400

    Rename variables & classnames for semantics

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
 function getUserAvatars() {
-    const input = document.getElementById("avatar-selector-input");
+    const userAvatarInput = document.getElementById("user-avatar-input");
     const avatarPreviews = document.getElementById("avatar-previews");
-  
+
     function handleAvatarChange() {
-      const userAvatarFiles = input.files;
-      if (!userAvatarFiles) return;
-  
-      for (let i = 0; i < avatarPreviews.children.length; i++) {
-        const avatarPreview = avatarPreviews.children[i];
-        const file = userAvatarFiles[i];
-        if (!file) {
-          avatarPreview.style.backgroundImage = "";
-          avatarPreview.classList.add("placeholder");
-          continue;
+        const userAvatarFiles = userAvatarInput.files;
+        if (!userAvatarFiles) return;
+
+        for (let i = 0; i < avatarPreviews.children.length; i++) {
+            const avatarPreview = avatarPreviews.children[i];
+            const file = userAvatarFiles[i];
+            if (!file) {
+                avatarPreview.style.backgroundImage = "";
+                avatarPreview.classList.add("placeholder");
+                continue;
+            }
+
+            const reader = new FileReader();
+            reader.addEventListener("load", () => {
+                avatarPreview.classList.remove("placeholder");
+                avatarPreview.style.backgroundImage = `url(${reader.result})`;
+            });
+            reader.readAsDataURL(file);
         }
-  
-        const reader = new FileReader();
-        reader.addEventListener("load", () => {
-          avatarPreview.classList.remove("placeholder");
-          avatarPreview.style.backgroundImage = `url(${reader.result})`;
-        });
-        reader.readAsDataURL(file);
-      }
     }
-  
-    input.addEventListener("change", handleAvatarChange);
-  }
-  
-  document.addEventListener("DOMContentLoaded", getUserAvatars);
-  
+
+    userAvatarInput.addEventListener("change", handleAvatarChange);
+}
+
+document.addEventListener("DOMContentLoaded", getUserAvatars);
