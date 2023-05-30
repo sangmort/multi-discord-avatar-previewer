837b9d0b01c924190fbbf38282a5e557900a68f4
sangmort <22422853+sangmort@users.noreply.github.com>
2023-05-30 17:40:14 -0400

    Refactor script to replace placeholders one-by-one

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 function getUserAvatars() {
     const userAvatarInput = document.getElementById("user-avatar-input");
-    const avatarPreviewLight = document.getElementById(
-        "avatar-preview-light"
-    );
+    const avatarPreviewLight = document.getElementById("avatar-preview-light");
     const avatarPreviewDark = document.getElementById("avatar-preview-dark");
 
     function handleAvatarChange() {
         const userAvatarFiles = userAvatarInput.files;
         if (!userAvatarFiles) return;
 
-        avatarPreviewLight.innerHTML = "";
-        avatarPreviewDark.innerHTML = "";
+        // Get all the placeholders for each theme
+        const placeholdersLight = document.querySelectorAll(
+            ".avatar.placeholder"
+        );
+        const placeholdersDark = document.querySelectorAll(
+            ".theme-container.dark-theme .avatar.placeholder"
+        );
 
         for (let i = 0; i < userAvatarFiles.length; i++) {
             const file = userAvatarFiles[i];
@@ -18,7 +21,15 @@ function getUserAvatars() {
             reader.addEventListener("load", () => {
                 const avatarPreview = createAvatarPreview(file, reader.result);
                 avatarPreviewLight.appendChild(avatarPreview.cloneNode(true));
-                avatarPreviewDark.appendChild(avatarPreview);
+                avatarPreviewDark.appendChild(avatarPreview.cloneNode(true));
+
+                // Remove the corresponding placeholders
+                if (placeholdersLight[i]) {
+                    placeholdersLight[i].remove();
+                }
+                if (placeholdersDark[i]) {
+                    placeholdersDark[i].remove();
+                }
             });
             reader.readAsDataURL(file);
         }
