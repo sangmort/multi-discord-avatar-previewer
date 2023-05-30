a3d60e08a579e6e00c925d65163f05fb44c6f604
sangmort <22422853+sangmort@users.noreply.github.com>
2023-05-30 16:34:01 -0400

    Refactor to allow placeholders before upload

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
 function getUserAvatars() {
     const userAvatarInput = document.getElementById("user-avatar-input");
-    const avatarPreviews = document.getElementById("avatar-previews");
+    const avatarPreviewLight = document.getElementById(
+        "avatar-preview-light"
+    );
+    const avatarPreviewDark = document.getElementById("avatar-preview-dark");
 
     function handleAvatarChange() {
         const userAvatarFiles = userAvatarInput.files;
         if (!userAvatarFiles) return;
 
-        for (let i = 0; i < avatarPreviews.children.length; i++) {
-            const avatarPreview = avatarPreviews.children[i];
-            const file = userAvatarFiles[i];
-            if (!file) {
-                avatarPreview.style.backgroundImage = "";
-                avatarPreview.classList.add("placeholder");
-                continue;
-            }
+        avatarPreviewLight.innerHTML = "";
+        avatarPreviewDark.innerHTML = "";
 
+        for (let i = 0; i < userAvatarFiles.length; i++) {
+            const file = userAvatarFiles[i];
             const reader = new FileReader();
             reader.addEventListener("load", () => {
-                avatarPreview.classList.remove("placeholder");
-                avatarPreview.style.backgroundImage = `url(${reader.result})`;
+                const avatarPreview = createAvatarPreview(file, reader.result);
+                avatarPreviewLight.appendChild(avatarPreview.cloneNode(true));
+                avatarPreviewDark.appendChild(avatarPreview);
             });
             reader.readAsDataURL(file);
         }
+    }
+
+    function createAvatarPreview(file, imageUrl) {
+        const avatarPreview = document.createElement("div");
+        avatarPreview.classList.add("avatar");
+        avatarPreview.style.backgroundImage = `url(${imageUrl})`;
+
+        if (!file) {
+            avatarPreview.classList.add("placeholder");
+        }
+
+        return avatarPreview;
     }
 
     userAvatarInput.addEventListener("change", handleAvatarChange);
