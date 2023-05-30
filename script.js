7cb06f4cfd2b09f955e6a4a1d1a5db697ef4bcc8
sangmort <22422853+sangmort@users.noreply.github.com>
2023-05-30 19:41:05 -0400

    Simplify code for better readability & performance

--- script.js
+++ script.js
@@ -1,70 +1,56 @@
 document.addEventListener("DOMContentLoaded", function () {
-    function generateAvatarPreview() {
-        const userAvatarInput = document.getElementById("user-avatar-input");
-        const avatarPreviewLight = document.getElementById(
-            "avatar-preview-light"
+    const userAvatarInput = document.getElementById("user-avatar-input");
+    const avatarPreviewLight = document.getElementById("avatar-preview-light");
+    const avatarPreviewDark = document.getElementById("avatar-preview-dark");
+
+    userAvatarInput.addEventListener("change", handleAvatarChange);
+
+    function handleAvatarChange() {
+        const userAvatarFiles = userAvatarInput.files;
+        if (!userAvatarFiles) return;
+
+        const placeholdersLight = document.querySelectorAll(
+            ".avatar.placeholder"
         );
-        const avatarPreviewDark = document.getElementById(
-            "avatar-preview-dark"
+        const placeholdersDark = document.querySelectorAll(
+            ".theme-container.dark-theme .avatar.placeholder"
         );
 
-        userAvatarInput.addEventListener("change", handleAvatarChange);
+        Array.from(userAvatarFiles).forEach((file, i) => {
+            const reader = new FileReader();
+            reader.addEventListener("load", () => {
+                const avatarPreview = createAvatarPreview(file, reader.result);
+                prependAvatarPreview(avatarPreview, avatarPreviewLight);
+                prependAvatarPreview(avatarPreview, avatarPreviewDark);
 
-        function handleAvatarChange() {
-            const userAvatarFiles = userAvatarInput.files;
-            if (!userAvatarFiles) return;
-
-            const placeholdersLight = document.querySelectorAll(
-                ".avatar.placeholder"
-            );
-            const placeholdersDark = document.querySelectorAll(
-                ".theme-container.dark-theme .avatar.placeholder"
-            );
-
-            Array.from(userAvatarFiles).forEach((file, i) => {
-                const reader = new FileReader();
-                reader.addEventListener("load", () => {
-                    const avatarPreview = createAvatarPreview(
-                        file,
-                        reader.result
-                    );
-                    prependAvatarPreview(avatarPreview, avatarPreviewLight);
-                    prependAvatarPreview(avatarPreview, avatarPreviewDark);
-
-                    removePlaceholder(placeholdersLight[i]);
-                    removePlaceholder(placeholdersDark[i]);
-                });
-                reader.readAsDataURL(file);
+                removePlaceholder(placeholdersLight[i]);
+                removePlaceholder(placeholdersDark[i]);
             });
-        }
+            reader.readAsDataURL(file);
+        });
+    }
 
-        function createAvatarPreview(file, imageUrl) {
-            const avatarPreview = document.createElement("div");
-            avatarPreview.classList.add("avatar");
-            avatarPreview.style.backgroundImage = `url(${imageUrl})`;
-            avatarPreview.classList.toggle("placeholder", !file);
+    function createAvatarPreview(file, imageUrl) {
+        const avatarPreview = document.createElement("div");
+        avatarPreview.classList.add("avatar");
+        avatarPreview.style.backgroundImage = `url(${imageUrl})`;
+        avatarPreview.classList.toggle("placeholder", !file);
 
-            return avatarPreview;
-        }
+        return avatarPreview;
+    }
 
-        function prependAvatarPreview(avatarPreview, container) {
-            const clonedPreview = cloneAvatarPreview(avatarPreview);
-            container.insertBefore(clonedPreview, container.firstChild);
-        }
+    function prependAvatarPreview(avatarPreview, container) {
+        const clonedPreview = avatarPreview.cloneNode(true);
+        container.insertBefore(clonedPreview, container.firstChild);
+    }
 
-        function cloneAvatarPreview(avatarPreview) {
-            return avatarPreview.cloneNode(true);
-        }
-
-        function removePlaceholder(placeholder) {
-            if (placeholder) {
-                placeholder.remove();
-            }
+    function removePlaceholder(placeholder) {
+        if (placeholder) {
+            placeholder.remove();
         }
     }
 
     function removeAvatarPreview() {
-        const userAvatarInput = document.getElementById("user-avatar-input");
         if (userAvatarInput.files.length < 1) {
             alert("No files uploaded");
             return;
@@ -83,7 +69,6 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
-    generateAvatarPreview();
     document
         .getElementById("remove-previews-button")
         .addEventListener("click", removeAvatarPreview);
