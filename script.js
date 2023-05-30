74fc7d1229c4d97bef26bf0c37356bcc9476daf3
sangmort <22422853+sangmort@users.noreply.github.com>
2023-05-30 14:54:26 -0400

    Rename `file` to `userAvatarFile` for semantics

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ function getUserAvatars() {
     const avatarPreview = document.getElementById("avatar-target");
 
     function handleAvatarChange() {
-        const file = input.files[0];
-        if (!file) return;
+        const userAvatarFile = input.files[0];
+        if (!userAvatarFile) return;
 
         const reader = new FileReader();
         reader.addEventListener("load", () => {
             avatarPreview.classList.remove("placeholder");
             avatarPreview.style.backgroundImage = `url(${reader.result})`;
         });
-        reader.readAsDataURL(file);
+        reader.readAsDataURL(userAvatarFile);
     }
 
     input.addEventListener("change", handleAvatarChange);
