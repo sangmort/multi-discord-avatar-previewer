90c71886dbd4760a394ac9ff828be73d1a2d46a7
sangmort <22422853+sangmort@users.noreply.github.com>
2023-05-30 15:32:34 -0400

    Refactor to allow 10 image upload + basic styling

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 function getUserAvatars() {
     const input = document.getElementById("avatar-selector-input");
-    const avatarPreview = document.getElementById("avatar-preview");
-
+    const avatarPreviews = document.getElementById("avatar-previews");
+  
     function handleAvatarChange() {
-        const userAvatarFiles = input.files[0];
-        if (!userAvatarFiles) return;
-
+      const userAvatarFiles = input.files;
+      if (!userAvatarFiles) return;
+  
+      for (let i = 0; i < avatarPreviews.children.length; i++) {
+        const avatarPreview = avatarPreviews.children[i];
+        const file = userAvatarFiles[i];
+        if (!file) {
+          avatarPreview.style.backgroundImage = "";
+          avatarPreview.classList.add("placeholder");
+          continue;
+        }
+  
         const reader = new FileReader();
         reader.addEventListener("load", () => {
-            avatarPreview.classList.remove("placeholder");
-            avatarPreview.style.backgroundImage = `url(${reader.result})`;
+          avatarPreview.classList.remove("placeholder");
+          avatarPreview.style.backgroundImage = `url(${reader.result})`;
         });
-        reader.readAsDataURL(userAvatarFiles);
+        reader.readAsDataURL(file);
+      }
     }
-
+  
     input.addEventListener("change", handleAvatarChange);
-}
-
-document.addEventListener("DOMContentLoaded", getUserAvatars);
+  }
+  
+  document.addEventListener("DOMContentLoaded", getUserAvatars);
+  
