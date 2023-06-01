6ae5820290f0fc2eef729ae7ac62d9e2d20b41a4
sangmort <22422853+sangmort@users.noreply.github.com>
2023-06-01 17:33:09 -0400

    Refactor constants, error handling, and functions

--- script.js
+++ script.js
@@ -2,43 +2,32 @@ document.addEventListener("DOMContentLoaded", function () {
     const userAvatarInput = document.getElementById("user-avatar-input");
     const avatarPreviewLight = document.getElementById("avatar-preview-light");
     const avatarPreviewDark = document.getElementById("avatar-preview-dark");
-
+    const maxFiles = 10;
+    let avatarPreviewsCount = 0;
     userAvatarInput.addEventListener("change", handleAvatarChange);
 
     function handleAvatarChange() {
-        const userAvatarFiles = userAvatarInput.files;
-        const maxFiles = 10;
-        const filesToProcess = Array.from(userAvatarFiles).slice(0, maxFiles);
-
-        const placeholdersLight = document.querySelectorAll(
-            ".avatar.placeholder"
-        );
-        const placeholdersDark = document.querySelectorAll(
-            ".theme-container.dark-theme .avatar.placeholder"
-        );
-
-        if (!userAvatarFiles) return;
-
+        const userAvatarFiles = Array.from(userAvatarInput.files).slice(0, maxFiles - avatarPreviewsCount);
+        if (userAvatarFiles.length === 0) return;
         if (userAvatarFiles.length > maxFiles) {
-            return alert(
-                "You can only upload a maximum of " + maxFiles + " files."
-            );
-        } else {
-            filesToProcess.forEach((file, i) => {
-                const reader = new FileReader();
-                reader.addEventListener("load", () => {
-                    const avatarPreview = createAvatarPreview(
-                        file,
-                        reader.result
-                    );
-                    prependAvatarPreview(avatarPreview, avatarPreviewLight);
-                    prependAvatarPreview(avatarPreview, avatarPreviewDark);
-
-                    removePlaceholder(placeholdersLight[i]);
-                    removePlaceholder(placeholdersDark[i]);
-                });
-                reader.readAsDataURL(file);
+            showAlert("You can only upload a maximum of " + maxFiles + " files.");
+            clearFileInput();
+            return;
+        }
+        userAvatarFiles.forEach((file, i) => {
+            if (avatarPreviewsCount >= maxFiles) return;
+            const reader = new FileReader();
+            reader.addEventListener("load", () => {
+                const avatarPreview = createAvatarPreview(file, reader.result);
+                prependAvatarPreview(avatarPreview, avatarPreviewLight);
+                prependAvatarPreview(avatarPreview, avatarPreviewDark);
+                removePlaceholder(i);
+                avatarPreviewsCount++;
             });
+            reader.readAsDataURL(file);
+        });
+        if (avatarPreviewsCount + userAvatarFiles.length >= maxFiles) {
+            disableFileInput();
         }
     }
 
@@ -47,7 +36,6 @@ document.addEventListener("DOMContentLoaded", function () {
         avatarPreview.classList.add("avatar");
         avatarPreview.style.backgroundImage = `url(${imageUrl})`;
         avatarPreview.classList.toggle("placeholder", !file);
-
         return avatarPreview;
     }
 
@@ -56,32 +44,48 @@ document.addEventListener("DOMContentLoaded", function () {
         container.insertBefore(clonedPreview, container.firstChild);
     }
 
-    function removePlaceholder(placeholder) {
-        if (placeholder) {
-            placeholder.remove();
+    function removePlaceholder(index) {
+        const placeholdersLight = document.querySelectorAll(".avatar.placeholder");
+        const placeholdersDark = document.querySelectorAll(".theme-container.dark-theme .avatar.placeholder");
+        removeElement(placeholdersLight[index]);
+        removeElement(placeholdersDark[index]);
+    }
+
+    function removeElement(element) {
+        if (element) {
+            element.remove();
         }
+    }
+
+    function disableFileInput() {
+        userAvatarInput.disabled = true;
+    }
+
+    function clearFileInput() {
+        userAvatarInput.value = "";
+    }
+
+    function showAlert(message) {
+        alert(message);
     }
 
     function removeAvatarPreview() {
         if (userAvatarInput.files.length < 1) {
-            alert("No files uploaded");
+            showAlert("No files uploaded");
             return;
         }
-
         const avatarPreviews = document.querySelectorAll(".avatar");
         avatarPreviews.forEach((preview) => {
             preview.remove();
         });
-
-        const placeholderAvatars = document.querySelectorAll(
-            ".avatar.placeholder"
-        );
+        const placeholderAvatars = document.querySelectorAll(".avatar.placeholder");
         placeholderAvatars.forEach((placeholder) => {
             placeholder.style.display = "block";
         });
+        avatarPreviewsCount = 0;
+        clearFileInput();
+        userAvatarInput.disabled = false;
     }
-
-    document
-        .getElementById("remove-previews-button")
-        .addEventListener("click", removeAvatarPreview);
+    const removePreviewsButton = document.getElementById("remove-previews-button");
+    removePreviewsButton.addEventListener("click", removeAvatarPreview);
 });
