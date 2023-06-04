117b9405be025edea6cb8e1c01594238e92f018a
sangmort <22422853+sangmort@users.noreply.github.com>
2023-06-03 23:53:45 -0400

    Refactor code for readability & remove redundancy

--- script.js
+++ script.js
@@ -2,22 +2,24 @@ document.addEventListener("DOMContentLoaded", function () {
     const userAvatarInput = document.getElementById("user-avatar-input");
     const avatarPreviewLight = document.getElementById("avatar-preview-light");
     const avatarPreviewDark = document.getElementById("avatar-preview-dark");
-    const avatarPreviews = document.querySelectorAll(".avatar");
-    const avatarPlaceholders = document.querySelectorAll(".avatar.placeholder");
     const maxFiles = 10;
     let avatarPreviewsCount = 0;
+
     userAvatarInput.addEventListener("change", handleAvatarChange);
 
     function handleAvatarChange() {
         const userAvatarFiles = userAvatarInput.files;
         if (!userAvatarFiles) return;
+
         const filesToProcess = Array.from(userAvatarFiles).slice(0, maxFiles - avatarPreviewsCount);
         const placeholdersLight = document.querySelectorAll(".avatar.placeholder");
         const placeholdersDark = document.querySelectorAll(".dark-theme .avatar.placeholder");
+
         filesToProcess.forEach((file, i) => {
             if (avatarPreviewsCount >= maxFiles) {
                 return;
             }
+
             const reader = new FileReader();
             reader.addEventListener("load", () => {
                 const avatarPreview = createAvatarPreview(file, reader.result, i);
@@ -26,11 +28,13 @@ document.addEventListener("DOMContentLoaded", function () {
                 removePlaceholder(placeholdersLight[i]);
                 removePlaceholder(placeholdersDark[i]);
                 avatarPreviewsCount++;
+
                 if (avatarPreviewsCount === maxFiles) {
                     showAlert("Only 10 file previews at a time, reset to add more.");
                     userAvatarInput.disabled = true;
                 }
             });
+
             reader.readAsDataURL(file);
         });
     }
@@ -46,12 +50,14 @@ document.addEventListener("DOMContentLoaded", function () {
         avatarPreview.classList.add("avatar");
         avatarPreview.style.backgroundImage = `url(${imageUrl})`;
         avatarPreview.classList.toggle("placeholder", !file);
+
         const removeButton = document.createElement("button");
         removeButton.classList.add("remove-button");
         removeButton.textContent = "remove";
         removeButton.dataset.index = index;
         removeButton.addEventListener("click", handleRemoveButtonClick);
         avatarPreview.appendChild(removeButton);
+
         return avatarPreview;
     }
 
@@ -61,32 +67,32 @@ document.addEventListener("DOMContentLoaded", function () {
         const index = Array.from(avatarPreview.parentNode.children).indexOf(avatarPreview);
         const lightThemeIndex = Array.from(avatarPreviewLight.children).indexOf(avatarPreview);
         const darkThemeIndex = Array.from(avatarPreviewDark.children).indexOf(avatarPreview);
+
         removeAvatarPreview(avatarPreview, index);
-        if (lightThemeIndex !== -1) {
-            const lightThemePreview = avatarPreviewLight.children[lightThemeIndex];
-            removeAvatarPreview(lightThemePreview, lightThemeIndex);
-        }
-        if (darkThemeIndex !== -1) {
-            const darkThemePreview = avatarPreviewDark.children[darkThemeIndex];
-            removeAvatarPreview(darkThemePreview, darkThemeIndex);
-        }
+        removeAvatarPreview(avatarPreviewLight.children[lightThemeIndex], lightThemeIndex);
+        removeAvatarPreview(avatarPreviewDark.children[darkThemeIndex], darkThemeIndex);
     }
 
     function removeAvatarPreview(avatarPreview, index) {
-        const lightThemePreview = avatarPreviewLight.children[index];
-        const darkThemePreview = avatarPreviewDark.children[index];
         const placeholder = createPlaceholder(index);
         avatarPreview.replaceWith(placeholder);
+
+        const lightThemePreview = avatarPreviewLight.children[index];
         if (lightThemePreview) {
             lightThemePreview.replaceWith(placeholder.cloneNode(true));
         }
+
+        const darkThemePreview = avatarPreviewDark.children[index];
         if (darkThemePreview) {
             darkThemePreview.replaceWith(placeholder.cloneNode(true));
         }
+
         avatarPreviewsCount--;
+
         if (avatarPreviewsCount < maxFiles) {
             userAvatarInput.disabled = false;
         }
+
         const remainingPreviews = document.querySelectorAll(".avatar");
         for (let i = index; i < remainingPreviews.length; i++) {
             remainingPreviews[i].querySelector(".remove-button").dataset.index = i;
@@ -113,6 +119,7 @@ document.addEventListener("DOMContentLoaded", function () {
             showAlert("No files uploaded");
             return;
         }
+
         toggleAvatars();
         avatarPreviewsCount = 0;
         userAvatarInput.value = "";
@@ -120,13 +127,15 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function toggleAvatars() {
-        avatarPreviews.forEach((preview) => {
+        document.querySelectorAll(".avatar").forEach((preview) => {
             preview.remove();
         });
-        avatarPlaceholders.forEach((placeholder) => {
+
+        document.querySelectorAll(".avatar.placeholder").forEach((placeholder) => {
             placeholder.style.display = "block";
         });
     }
+
     document.getElementById("remove-previews-button").addEventListener("click", removeAllAvatarPreviews);
 
     function showAlert(message) {
